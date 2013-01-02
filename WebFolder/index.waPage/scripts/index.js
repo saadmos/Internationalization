1a493ceeaa3e43c6f235960595a30f296259be00
Saad Mousliki <saadmos06@gmail.com>
2013-01-02 11:53:50 +0000

    first commit of internationalization test app

---
+++ WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,48 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var combobox1 = {};	// @combobox
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	combobox1.change = function combobox1_change (event)// @startlock
+	{// @endlock
+        debugger;
+        var abrv = sources.country.abbreviation;
+		modifyLang(abrv);
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		
+	};// @lock
+	
+	function modifyLang(abrv){
+		
+		jQuery.i18n.properties({
+    		name: 'Messages',
+    		path: '../bundle/',
+    		mode: 'both',
+    		language: abrv,
+    		callback: function() {
+      
+        $$('welcomeText').setValue($.i18n.prop('msg_Welcome'));
+        $$("chooseText").setValue($.i18n.prop('msg_choose'));
+        $$('Title1').setValue($.i18n.prop('msg_home_title'));
+        $$('Paragraph').setValue($.i18n.prop('msg_home_paragraph'));
+      
+    }
+});
+		
+		
+		
+		}
+
+// @region eventManager// @startlock
+	WAF.addListener("combobox1", "change", combobox1.change, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
